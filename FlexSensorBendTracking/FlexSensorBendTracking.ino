b1a068424f5ce37681134c42d841d61a69f64cba
Tyler Burns <tibsdoe14@gmail.com>
2016-01-17 00:59:42 -0500

    Changed back to delimiter
    Changed back to delimiter system

--- FlexSensorBendTracking/FlexSensorBendTracking.ino
+++ FlexSensorBendTracking/FlexSensorBendTracking.ino
@@ -1,3 +1,7 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <IRremoteTools.h>
+
 /* Arduino Flex Sensor Bend Tracking 
 *  
 * Split circuit involving a 2200 ohm resistor 
@@ -21,26 +25,19 @@ void setup()
 // LOOP 
 void loop() 
 {
-  // Reading th e values from the analog 
+  // Reading the values from the analog 
   int flexSensorReading = analogRead(flexSensorPin); // Flex sensor pin 
   int flexSensorReading2 = analogRead(flexSensorPin2); // Flex sensor pin 2
   int flexSensorReading3 = analogRead(flexSensorPin3); // Flex sensor pin 3
   // Serial.println(flexSensorReading); // Print out the raw reading 
 
-  // Remap the raw flex-sensor value to a new value 
-  int flex0to100 = map(flexSensorReading, 30, 180, 90, 0);
-  int flex0to100_2 = map(flexSensorReading2, 30, 180, 90, 0); 
-  int flex0to100_3 = map(flexSensorReading3, 30, 180, 90, 0); 
-
-  // One's digit is the value we care about 
-  flex0to100 = flex0to100 * 10 + 1; // ends in 1 
-  flex0to100_2 = flex0to100_2 * 10 + 2; // ends in 2 
-  flex0to100_3 = flex0to100_3 * 10 + 3; // ends in 3 
-
   // Print values 
-  Serial.println(flex0to100); // A0  
-  Serial.println(flex0to100_2); // A1
-  Serial.println(flex0to100_3); // A2
+  Serial.print(flexSensorReading); // A0  
+  Serial.print(","); //delimiter
+  Serial.print(flexSensorReading2); // A1
+  Serial.print(","); //delimiter
+  Serial.print(flexSensorReading3); // A2
+  Serial.println(","); //delimiter
 
   // Delay 
   delay(25); // Slow the output so we can detect flexing more continuously 
