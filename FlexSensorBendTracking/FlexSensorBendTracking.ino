cf3e82cd653cba74b0e5a6b8c69425a6f1189ae6
Jma353 <jma353@cornell.edu>
2016-01-16 23:33:49 -0500

    2 hand, 1's digit determining which strip is bending

--- FlexSensorBendTracking/FlexSensorBendTracking.ino
+++ FlexSensorBendTracking/FlexSensorBendTracking.ino
@@ -5,7 +5,8 @@
 */
 
 
-int flexSensorPin = A0; // The flex sensor is pin 0 
+int flexSensorPin = A0; // The flex sensor for pin 0 
+int flexSensorPin2 = A1; // The flex sensor for pin 1
 
 // 100 == all the way bent 
 // 0 == not bent at all 
@@ -20,13 +21,16 @@ void setup()
 void loop() 
 {
   int flexSensorReading = analogRead(flexSensorPin); // Flex sensor pin 
-
+  int flexSensorReading2 = analogRead(flexSensorPin2); // Flex sensor pin 2
   // Serial.println(flexSensorReading); // Print out the raw reading 
 
   // Remap the raw flex-sensor value to a new value 
-  int flex0to100 = map(flexSensorReading, 30, 180, 90, 0); 
+  int flex0to100 = map(flexSensorReading, 30, 180, 90, 0);
+  int flex0to100_2 = map(flexSensorReading2, 30, 180, 90, 0); 
+  flex0to100 = flex0to100/10 * 10 + 1; 
+  flex0to100_2 = flex0to100_2/10 * 10 + 2; 
   Serial.println(flex0to100); // Print out this new value 
-
+  Serial.println(flex0to100_2); // Print out this new value 
   delay(25); // Slow the output so we can detect flexing more continuously 
 
     
